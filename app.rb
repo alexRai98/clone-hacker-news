b568aa5fbcc2bddf8fe3cbbe060cddbd0f71ff26
Rai <raikcuerda19@gmail.com>
2020-05-05 16:25:12 -0500

    finish clone HN

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require "sinatra"
 require "sinatra/reloader" if development?
 require "time_difference"
+require "json"
 
 helpers do
   # Takes a time object and returns how much time has passed
@@ -14,4 +15,18 @@ end
 get "/" do
   test_time = Time.now - (60 * 5) # 5 minutes ago
   "#{time_since_in_words(test_time)} ago"
+end
+
+get "/hackerNews" do
+  file= File.read("stories.json")
+  @stories = JSON.parse(file)
+  @stories.map{|post|
+    time = Time.now - post["time"]
+    post["time_lg"] = time_since_in_words(time).split(",")[0]
+  }
+  @stories.map{|post|
+    url = post["url"].match(/https?:\/\/(?<path>[\w.]+)\/.+/)
+    post["urls"] = url[:path]
+  }
+  erb :index
 end
